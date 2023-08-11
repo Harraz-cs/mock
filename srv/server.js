fc33112e405ed5492e9f9e3cc37a03159b8e6735
Harraz-cs <harraz@rajang.com>
2023-08-11 10:49:22 +0800

    added values yaml and a helm hook
    Helm hook is to execute "cds deploy" inside the sap cap service pods

--- srv/server.js
+++ srv/server.js
@@ -13,6 +13,24 @@ cds.on("bootstrap", (app) => {
     console.error(err.stack); // Log error stack trace
     res.status(500).send("Oops! The server broke.");
   });
+
+  // Add custom route to fetch policies
+  app.get("/api/policies", async (req, res) => {
+    const pretty = req.query.pretty;
+
+    try {
+      const response = await axios.get(process.env.OPA_URL + "/v1/policies", {
+        params: {
+          pretty: pretty === "true", // ensure pretty is a boolean
+        },
+      });
+
+      res.json(response.data);
+    } catch (error) {
+      console.error(`Error: ${error}`);
+      res.status(500).send("Error fetching policies");
+    }
+  });
 });
 
 cds.on("served", async () => {
